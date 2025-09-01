e4916bf308b55a714f283caee007aab6d6b8f7fd
kyany-cany <yassy.can@gmail.com>
2025-09-01 16:23:51 +0900

    fix delete v2

--- front/src/pages/consent.tsx
+++ front/src/pages/consent.tsx
@@ -6,7 +6,7 @@ import { API_BASE } from "@/lib/config";
 const CONSENT_VERSION = "v1";
 
 export async function saveResearchConsent(consented: boolean, version = "v1"): Promise<void> {
-    const res = await apiFetch(`${API_BASE}/v2/consent`, {
+    const res = await apiFetch(`${API_BASE}/consent`, {
         method: "POST",
         body: JSON.stringify({
             research_consent: consented,
