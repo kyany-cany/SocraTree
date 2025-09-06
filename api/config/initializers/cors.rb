71c6a46f948258810bbe6f726f765f4e15652f0b
kyany-cany <yassy.can@gmail.com>
2025-09-06 23:49:45 +0900

    refactore cors

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -8,7 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins ENV.fetch("CORS_ORIGINS", "http://localhost:5173")
-    resource "/users/*",
+    resource %r{^/(users(?:/.*)?|csrf)$},
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
       credentials: true
